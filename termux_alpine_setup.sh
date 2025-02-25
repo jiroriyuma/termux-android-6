af4e740494bc78bd06c53d262386a7252f46cf00
JiroRiyuma <jiroriyuma@gmail.com>
2025-02-25 16:41:34 +0800

    .

---
+++ termux_alpine_setup.sh
@@ -0,0 +1,40 @@
+# installing bash
+bashrc=$HOME/.bash.bashrc
+apk add bash
+echo "exec bash" >> ~/.profile
+echo "PS1='\[\e[1;32m\][\u@\h \w] \\$ \[\e[0m\]'" >> $bashrc # [u0_a115@localhost ~] $
+
+# recommended packages
+apk add git nano
+
+# setup vs code ssh server (no password setup). to connect "ssh -p 8023 root@<ip-address>"
+apk add gcompat libstdc++ openssh openssh-server
+ssh-keygen -A
+passwd -d root
+
+echo "/usr/sbin/sshd -p 8023" >> $bashrc # setup autorun
+
+ssh_config_file="/etc/ssh/sshd_config"
+echo "PermitEmptyPasswords yes" >> $ssh_config_file
+echo "PasswordAuthentication yes" >> $ssh_config_file
+echo "PermitRootLogin yes" >> $ssh_config_file
+echo "AllowTcpForwarding yes" >> $ssh_config_file
+echo "GatewayPorts yes" >> $ssh_config_file
+echo "PermitTunnel yes" >> $ssh_config_file
+
+# install programming languages support  (this takes an hour to install)
+
+# C/C++
+apk add gcc g++ make musl-dev
+
+# python3
+apk add python3 py3-pip
+
+# nodejs
+apk add nodejs npm
+
+# java
+apk add openjdk17
+export JAVA_HOME="/usr/lib/jvm/java-17-openjdk"
+echo "JAVA_HOME=$JAVA_HOME" >> $BASHRC
+echo "PATH=$JAVA_HOME/bin:$PATH" >> $BASHRC
---
+++ termux_setup.sh
@@ -0,0 +1,23 @@
+termux-setup-storage
+
+# fix issues with repositories
+rm $PREFIX/etc/apt/sources.list.d/* &>/dev/null 2>&1
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://packages-cf.termux.org/termux-main-21 stable main@' $PREFIX/etc/apt/sources.list
+apt update && apt upgrade -y
+
+# essensial packages
+pkg -y install sopenssh nano git aria2
+
+# setting up bash
+bashrc=$HOME/../usr/etc/bash.bashrc
+
+# just replacing the old variables 
+echo 'clear' >> $bashrc # clear screen
+echo 'PROMPT_DIRTRIM=2' >> $bashrc # don't know
+echo "PS1='\[\e[1;32m\][\u@\h \w] \\$ \[\e[0m\]'" >> $bashrc # [u0_a115@localhost ~] $
+
+# install working distro since termux android 6 support was discontinued
+cd $HOME
+curl -LO https://raw.githubusercontent.com/Hax4us/TermuxAlpine/master/TermuxAlpine.sh
+bash TermuxAlpine.sh
+startalpine
